f06304dc955c6684704ca43057ab1061a3984b1d
Ramon Brand <ramon2009@gmail.com>
2022-04-18 21:39:42 +0300

    Add logic module

--- src/game.rs
+++ src/game.rs
@@ -4,6 +4,7 @@ pub struct Game {
     pub x: f32,
     pub y: f32,
 }
+
 impl Game {
     pub fn new() -> GameResult<Game> {
         Ok(Game { x: 50.0, y: 50.0 })
--- src/logic.rs
+++ src/logic.rs
@@ -1,9 +1,9 @@
 use crate::Game;
 use ggez::{Context, input};
-//use ggez::timer::delta;
+use ggez::timer::delta;
 
 pub fn update(ctx: &Context, game: &mut Game) {
-    //let delta = delta(ctx).as_secs_f32();
+    let delta = delta(ctx).as_secs_f32();
     let mouse_position = input::mouse::position(ctx);
     game.x = mouse_position.x;
     game.y = mouse_position.y;
