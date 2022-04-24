acb257e2b2d77918a2a5aa034f9bd5be35641154
Ramon Brand <ramon2009@gmail.com>
2022-04-24 12:06:00 +0300

    Add constraints

--- crates/logic/src/lib.rs
+++ crates/logic/src/lib.rs
@@ -12,7 +12,7 @@ pub fn init(ctx: &Context, state: &mut State) {
                 height: 160.0,
             },
             position: Vec2::new(50.0, 50.0),
-            velocity: Vec2::new(50.0, 50.0),
+            velocity: Vec2::default(),
         },
     );
     println!("{:?}", state);
@@ -22,6 +22,7 @@ pub fn update(ctx: &Context, state: &mut State) {
     let delta = delta(ctx).as_secs_f32();
     let mouse_position = input::mouse::position(ctx);
     update_physics(delta, state);
+    update_constraints(state);
 }
 
 fn update_physics(delta: f32, state: &mut State) {
@@ -29,3 +30,26 @@ fn update_physics(delta: f32, state: &mut State) {
         entity.position = entity.position + entity.velocity * delta;
     })
 }
+
+fn update_constraints(state: &mut State) {
+    state
+        .entities
+        .get_mut(&String::from("player"))
+        .inspect_mut(|entity| entity.position.x = 50.0);
+}
+
+trait InspectMut<T> {
+    fn inspect_mut<F>(&mut self, callback: F)
+    where
+        F: FnMut(&mut T);
+}
+
+impl<T> InspectMut<T> for Option<T> {
+    fn inspect_mut<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut T),
+    {
+        self.iter_mut().for_each(|i| callback(i));
+    }
+}
+
